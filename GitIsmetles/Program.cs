2ff97cde0c0cc3ae0335e99284d50504aea77f8c
Dániel Palman <palman.daniel47@gmail.com>
2020-09-08 23:35:50 +0200

    Ko papir ollo jatek kezdetleges de hibatlan mukodese

--- GitIsmetles/Program.cs
+++ GitIsmetles/Program.cs
@@ -15,13 +15,27 @@ namespace GitIsmetles
 
             int gepValasz = veletlen.Next(0, 3);
 
-            Console.WriteLine("Gép választása: {0}", lehetoseg[gepValasz]);
+            //Console.WriteLine("Gép választása: {0}", lehetoseg[gepValasz]);
 
             int jatekosValasz;
             Console.WriteLine("Kő(0), Papír(1), Olló(2)");
             Console.WriteLine("Válassz: ");
             jatekosValasz = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Játékos választása: {0}", lehetoseg[jatekosValasz]);
+            Console.WriteLine("Gép választása: {0}", lehetoseg[gepValasz]);
+
+            if (gepValasz == 0 && jatekosValasz == 1 || gepValasz == 1 && jatekosValasz == 2 || gepValasz == 2 && jatekosValasz == 0)
+            {
+                Console.WriteLine("Játékos nyert");
+            }
+            if (gepValasz == jatekosValasz)
+            {
+                Console.WriteLine("Döntetlen");
+            }
+            if (gepValasz == 0 && jatekosValasz == 2 || gepValasz == 1 && jatekosValasz == 0 || gepValasz == 2 && jatekosValasz == 1)
+            {
+                Console.WriteLine("Gép nyert");
+            }
 
             Console.ReadKey();
         }
